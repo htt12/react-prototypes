ebd23a88af2f4ff37692a34e5c8d5f64313f154b
htt12 <harrisontruong2@gmail.com>
2018-04-30 15:08:44 -0700

    Completed SGT layout and dynamic data creation

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,31 @@
+import React from 'react';
+import Table from './table'
 
+export default () => {
+
+    const students = [
+        {
+            student: 'Tommy',
+            course: 'Science',
+            grade: 94
+        },
+        {
+            student: 'Jennifer',
+            course: 'Biology',
+            grade: 44
+        },
+        {
+            student: 'Steven',
+            course: 'Math',
+            grade: 25
+        }
+    ]
+
+    return (
+        <div className='container'>
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )
+};
 
